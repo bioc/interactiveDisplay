8404f7823730b1f4c19521159e1410437ac9e10a
Shawn Balcome <balc0022@umn.edu>
2014-04-23 06:06:12 +0000

    Added a little function gridtweak().  It may be removed later, but I personally find it convenient.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/interactiveDisplay@89384 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/gridtweak.R
@@ -0,0 +1,59 @@
+
+setGeneric("gridtweak", function(...)
+  standardGeneric("gridtweak")
+)
+
+setMethod("gridtweak", 
+          signature(),
+          function(...){   
+            app <- list(
+              
+              ui =
+                sidebarLayout(
+                  sidebarPanel(
+                    shiny::tags$textarea(id="inbox", rows=4, cols=80, "gp <- ggplot() +
+geom_point(data=iris,aes(x=Sepal.Length,y=Petal.Length))
+print(gp)"),
+                    actionButton("gobutton", "Refresh Plot"),
+                    shiny::tags$style(type='text/css', "#inbox { width: 1000px; }")
+                  ),
+                  mainPanel( 
+                    shiny::tags$head(
+                      shiny::tags$style(type='text/css',
+                                        ".span8 { width: 100%; align: right; }")
+                    ),
+                    .jstags(),
+                    uiOutput("svgplot")
+                  )
+                ),
+              
+              server = function(input,output) {
+                output$svgplot <- renderUI({
+                  if (input$gobutton == 0)
+                    return()
+                  isolate({
+                    jscode <- "
+                    <script type='text/javascript'>
+                    $(document).ready(function() {
+                    $('svg').svgPan('viewport');
+                    });
+                    </script>
+                    "
+                    eval(parse(text=input$inbox))
+                    mysvg <- grid.export()
+                    mysvg2 <- saveXML(mysvg$svg[["g"]])
+                    mysvg3 <- sub("<g transform=",
+                                  "<g id='viewport' transform=",
+                                  mysvg2)                 
+                    mysvg4 <- sub(">NA<","><",mysvg3)
+                    htmlxml <- HTML(paste("<svg xmlns='http://www.w3.org/2000/svg'
+                                          xmlns:xlink='http://www.w3.org/1999/xlink' 
+                                          version='1.1' width='100%' height='100%'>",
+                                          jscode,mysvg4,"</svg>",sep=""))
+                    htmlxml 
+                  })
+                })
+              }
+                  )
+            runApp(app, ...)
+          })
