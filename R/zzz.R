3a183770d07b1a6009ba4379f92c560a324ae1fe
Shawn Balcome <balc0022@umn.edu>
2014-09-05 19:13:17 +0000

    Pulls from both resource paths of interactiveDisplayBase and interactiveDisplay now.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/interactiveDisplay@93873 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,7 @@
     suppressMessages({
         addResourcePath("js", system.file("www", "js", package="interactiveDisplayBase"))
         addResourcePath("css", system.file("www", "css", package="interactiveDisplayBase"))
+        addResourcePath("js", system.file("www", "js", package="interactiveDisplay"))
+        addResourcePath("css", system.file("www", "css", package="interactiveDisplay"))
     })
 }
