eadddaa8e246e15db5fc4d64382f2f609688232e
Joseph Paulson <jpaulson@umiacs.umd.edu>
2014-09-08 04:15:58 +0000

    Updating MRexperiment.R code
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/interactiveDisplay@93905 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/MRexperiment.R
+++ R/MRexperiment.R
@@ -2,43 +2,39 @@
 ###   metagenomeSeq
 ################################################################################
 
-heatcolor1 <- function(inputId1) {
-  tagList(
-    shiny::tags$input(id = inputId1, class = "color", value = "EDF8B1",
-      onchange = "window.Shiny.onInputChange('color1', this.color.toString())")
-  )
-}
-
-heatcolor2 <- function(inputId2) {
-  tagList(
-    shiny::tags$input(id = inputId2, class = "color", value = "7FCDBB",
-      onchange = "window.Shiny.onInputChange('color2', this.color.toString())")
-  )
-}
-
-heatcolor3 <- function(inputId3) {
-  tagList(
-    shiny::tags$input(id = inputId3, class = "color", value = "2C7FB8",
-      onchange = "window.Shiny.onInputChange('color3', this.color.toString())")
-  )
-}
-
 .MGS_setSidebarPanel <- function(){
   sidebarPanel(
     h3("MRexperiment", align = "center"),
     HTML("<hr />"),
-    checkboxInput("noheat","Suppress Heatmap"),
-    checkboxInput("flip","Transpose Heatmap"),
+    checkboxInput("norm","Normalize data (CSS-Normalization)",value=TRUE),
+    numericInput("pd","Phenotype column:",1,min=1),
     HTML("<hr />"),
-    radioButtons('rainbow', 'Heatmap Color Scale',
-                 c('Rainbow'='default',
-                   'Three Color'='tri'),
-                 'Rainbow'),
-    heatcolor1('exampleTextarea1'),
-    heatcolor2('exampleTextarea2'),
-    heatcolor3('exampleTextarea3'),
+    h4("Heatmap options", align = "center"),
+    numericInput("noFeatures","Number of features:",50,min=1,max =200),
+    radioButtons("heatMethod","Choose features by:",c("Median Absolute Deviation"="mad","Variability"="sd")),
+
     HTML("<hr />"),
-    actionButton("closebutton", "Stop/Save")
+    h4("Feature abundance options", align = "center"),
+    numericInput("featureIndex","Display feature:",1,min=1),
+    HTML("<hr />"),
+    h4("PCA/MDS options", align = "center"),
+    radioButtons("pcaOrMds","PCA or MDS:",c("PCA"="TRUE","MDS"="FALSE"),selected="TRUE"),
+    radioButtons("useDist","Make use of count distances:",c("False"="FALSE","True"="TRUE"),selected="FALSE"),
+    conditionalPanel(condition = "input.useDist == 'TRUE'",
+      selectInput("distance", "Distance:", 
+          choices=c("euclidean","manhattan","canberra","bray",
+            "kulczynski","jaccard","gower","altGower","morisita",
+            "horn","raup","binomial","chao","cao"),selected="euclidean")
+      ),
+    numericInput('dimensionx', 'X-axis dimension:', 1,
+         min = 1, max = 4),
+    numericInput('dimensiony', 'Y-axis dimension:', 2,
+         min = 1, max = 4),
+    HTML("<hr />"),
+    h4("Diversity options", align = "center"),
+    selectInput("diversity","Diversity index:",choices=c("shannon", "simpson", "invsimpson"))
+    # HTML("<hr />"),
+    # actionButton("closebutton", "Stop/Save")
   )
 }
 
@@ -69,27 +65,31 @@ heatcolor3 <- function(inputId3) {
     ),
     .loading_gif(),
     tabsetPanel(
-      tabPanel("Plot 1",
-               HTML("On-screen Documentation"),
+      tabPanel("Heatmap",
+               HTML("Heatmap of the abundances sorted by either MAD or SD"),
                HTML("<hr />"),
                plotOutput("plot1")),
-      tabPanel("Plot 2",
-               HTML("On-screen Documentation"),
+      tabPanel("Feature abundance",
+               HTML("Manhattan plots of bacterial abundances where each bar represents the abundance in a sample."),
                HTML("<hr />"),
                plotOutput("plot2")),
-      tabPanel("Plot 3",
-               HTML("On-screen Documentation"),
+      tabPanel("PCA/MDS",
+               HTML("PCA or MDS plots (methods for dimensionality-reduction). Projecting samples onto two dimensions and coloring the samples by phenotype."),
                HTML("<hr />"),
-               plotOutput("plot3"))
+               plotOutput("plot3")),
+      tabPanel("Diversity",
+               HTML("Shannon diversity index boxplots of the various stratified samples."),
+               HTML("<hr />"),
+               plotOutput("plot4"))
     ),
     tabsetPanel(
-      tabPanel("Table 1",
-               HTML("On-screen Documentation"),
+      tabPanel("Feature data",
+               HTML("Feature information"),
                HTML("<hr />"),
                dataTableOutput("table1")
       ),
-      tabPanel("Table 2",
-               HTML("On-screen Documentation"),
+      tabPanel("Phenotype data",
+               HTML("Phenotype information"),
                HTML("<hr />"),
                dataTableOutput("table2")
       )
@@ -102,7 +102,8 @@ setMethod("display",
   function(object, ...){
     
     .usePackage('metagenomeSeq')
-       
+    .usePackage('vegan')
+
     app <- list(
       ui =
         bootstrapPage(
@@ -111,118 +112,91 @@ setMethod("display",
         ),
       
       server = function(input, output){
-        
-        tmpdata <- reactive({
-          #  Generate some fake example data.
-          m <- matrix(rnorm(2600),nrow=26)
-          rownames(m) <- replicate(26,paste0(sample(letters,1),sample(1000,1)))
-          colnames(m) <- replicate(100,paste0(sample(LETTERS,1),sample(100000,1)))
-          return(m)
-        })
-                
+
+        data = reactive({
+                MRcounts(object,norm=input$norm,log=input$norm)
+          })
+        rawdata = reactive({
+                MRcounts(object)
+          })
+        pd   = reactive({
+                pd = pData(object)
+                if(ncol(pd)==0){
+                  return(as.matrix(rep(1,nrow(pd))))
+                } else {
+                  return(pd)
+                }
+          })
+
         output$plot1 <- renderPlot({
           
           #  Get data ready for ggplot
-          my_mat <- tmpdata()
-          melted <- melt(my_mat)
-          names(melted) <- c("Var1","Var2","value")
+          mat = data()
+          otusToKeep = which(rowSums(mat) > 0)
+          otuStats = apply(mat[otusToKeep, ], 1, input$heatMethod)
+          otuIndices = otusToKeep[order(otuStats, decreasing = TRUE)[1:input$noFeatures]]
+
+          my_mat <- mat[otuIndices,]
+          # melted <- melt(my_mat)
+          # names(melted) <- c("Var1","Var2","value")
           
-          melted$Var1 <- factor(melted$Var1, rownames(my_mat))
-          melted$Var2 <- factor(melted$Var2, colnames(my_mat))
-          
-          #  For that suppression option.
-          if(is.null(my_mat) || input$noheat == TRUE){
-            return()
-          }
-          #  This looks weird but it's a little trick to keep the color picker
-          #  from refreshing/loading unnecessarily...
-          else{    
-            if(input$rainbow == 'default'){
-              isolate({
-                c1 <- input$color1
-                c2 <- input$color2
-                c3 <- input$color3
-              })
-            }
-            else{
-              c1 <- input$color1
-              c2 <- input$color2
-              c3 <- input$color3
-            }
-          }
-          
-          #  Default colorblind friendly colors.
-          if(length(c1)==0){
-            c1 <- "EDF8B1"
-          }
-          if(length(c2)==0){
-            c2 <- "7FCDBB"
-          }
-          if(length(c3)==0){
-            c3 <- "2C7FB8"
-          }
-          
-          #  Full "rainbow" spectrum
-          if(input$rainbow=='default'){
-            myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
-          }
-          
-          #  Chosen tricolor palette
-          if(input$rainbow=='tri'){
-            myPalette <- colorRampPalette(rev(c(paste("#",toupper(c1),sep=""),
-                                                paste("#",toupper(c2),sep=""),
-                                                paste("#",toupper(c3),sep=""))))
-          }
-          
-          #  A lot of ggplot formatting...
-          #  There are many excellent heatplot libraries, but to have more
-          #  control I decided to built it from the ground up.
-          gp <- ggplot(melted, aes(x = Var2, y = Var1, fill = value))
-          gp <- gp + geom_tile()
-          gp <- gp + coord_fixed()
-          gp <- gp + scale_fill_gradientn(colours = myPalette(100))
-          gp <- gp + scale_x_discrete(expand = c(0, 0))
-          gp <- gp + scale_y_discrete(expand = c(0, 0))
-          gp <- gp + coord_equal()
-          gp <- gp + theme_bw()
-          gp <- gp + xlab("X label")
-          gp <- gp + ylab("Y label")
-          gp <- gp + theme(axis.ticks = element_blank(),
-                           axis.text.x = element_text(angle = -45,
-                                                      hjust = 0))
-          if(input$flip==TRUE){
-            gp <- gp + coord_flip() 
-          }
-          
-          return(print(gp))
-        })
+          # melted$Var1 <- factor(melted$Var1, rownames(my_mat))
+          # melted$Var2 <- factor(melted$Var2, colnames(my_mat))
+         
+          trials = pd()[,input$pd]
+          heatmapColColors=brewer.pal(12,"Set3")[as.integer(factor(trials))];
+          heatmapCols = colorRampPalette(brewer.pal(9, "RdBu"))(50)
+          heatmap.2(my_mat,trace="none",cexRow=.8,cexCol=.8,col = heatmapCols,ColSideColors = heatmapColColors)
+        },height=850)
                 
         output$plot2 <- renderPlot({
-          #Empty!
+          mat = data()
+          pdata  = factor(pd()[,input$pd])
+          ylabel = ifelse(input$norm,yes=expression(bold("Log"[2]*" Abundance")),no="No. raw reads")
+          plotFeature(mat,otuIndex = input$featureIndex,ylab=ylabel,main=rownames(mat)[input$featureIndex],
+          classIndex = list(All_samples=1:ncol(mat)),col=pdata,font.lab=2,font.axis=1,sort=FALSE,xaxt="n")
+          legend("topleft",legend=unique(pdata),fill=unique(pdata),box.col="NA")
         })
         
         output$plot3 <- renderPlot({
-          #Empty!
+          useDist = input$useDist
+          pdata  = factor(pd()[,input$pd])
+          plotOrd(data(),n=200,pch=21,bg=pdata,usePCA=input$pcaOrMds,
+            comp=c(input$dimensionx,input$dimensiony),
+            useDist=useDist,distfun=vegan::vegdist,dist.method=input$distance)
+          legend("topleft",levels(pdata),fill=factor(levels(pdata)),box.col="NA")
         })
-        
+
+        output$plot4 <- renderPlot({
+          pdata  = factor(pd()[,input$pd])
+          mat = t(rawdata())
+          H = vegan::diversity(mat,index=input$diversity)
+          boxplot(H~pdata,ylab=paste(input$diversity,"diversity index"))
+        })
+
+
         #  Example Table
         output$table1 <- renderDataTable({
-          return(mtcars)
-        })
-        
+          fd = fData(object)
+          Index = 1:nrow(fd)
+          Rownames = rownames(fd)
+          fd = cbind(Index,Rownames,fd)
+          return(fd)
+        },options = list(iDisplayLength = 10))
+                          
         #  Example Table
         output$table2 <- renderDataTable({
-          return(iris)
-        })
-                             
+          return(pData(object))
+        },options = list(iDisplayLength = 10))
+
         #  Close/Save Button, depending on your needs.
-        observe({
-          if (input$closebutton == 0)
-            return()
-          isolate({
-            stopApp(returnValue="Something you want to return to the R console")
-          })
-        })     
+        # observe({
+        #   if (input$closebutton == 0)
+        #     return()
+        #   isolate({
+        #     stopApp(returnValue="Something you want to return to the R console")
+        #   })
+        # })     
       })
     runApp(app, ...)
   })
